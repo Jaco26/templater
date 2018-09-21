71fa41d6998861e17d0db90a09cb618c8bd6312d
Jacob Albright <jacob.albright23@gmail.com>
2018-09-20 20:16:25 -0500

    working to evaluate methods called in template handlebars as well as javascript expressions in template handlebars

--- template-compiler.js
+++ template-compiler.js
@@ -4,7 +4,7 @@ const TemplateComp = (() => {
     eventHandler: /(?!@\w+=)"\w+(\(.+\)?)*>*"/g,
     args: /\(.+\)/g,
     tagsWithEvents: /<.*?@\w+=".+".*?>/g,
-    handleBar: /{{.+}}/g,
+    handleBar: /{{.+?}}/g,
     tagsWithJModels: /<.*?j-model=".+".*?>/g,
     jModel: /j-model="\w+"/g,
 
@@ -23,23 +23,63 @@ const TemplateComp = (() => {
     },
   };
 
-  function evaluateExpression(expression) {
-    if (this._methods[expression]) {
-      return this._methods[expression]();
+  function isMethod(exp, methods) {
+    let args;
+    const calledMethod = /\w+\(.*?\)/g;
+
+
+    if (methods[exp] || calledMethod.test(exp) && methods[exp.slice(0, exp.indexOf('('))]) {
+      console.log(methods[exp.slice(0, exp.indexOf('('))]);
+      console.log('GETTTTT', exp);
+
+      args = exp.match(/\(.+\)/g)[0].slice(1, -1).split(',').map(arg => {
+        if (Number(arg)) {
+          return Number(arg);
+        } else {
+          return arg.trim();
+        }
+      });
     }
+
+
+    // if (methods[exp] /* || methods[exp].slice(0, exp.indexOf('(') )*/ && toCall.test(exp)) {
+    //   args = exp.match(/\(.+\)/g)[0].slice(1, -1).split(',').map(arg =>  {
+    //     if (Number(arg)) {
+    //       return Number(arg);
+    //     } else {
+    //       return arg.trim();
+    //     }
+    //   });
+    // }
+    console.log(args);
+  }
+
+  function isData(exp, data) {
+
+  }
+
+  function evaluateExpression(expression) {
+
+    isMethod(expression, this._methods);
+
     if (this._data[expression]) {
       return this._data[expression];
     }
 
+    if (this._methods[expression]) {
+      return this._methods[expression]();
+    }
 
+    return eval(expression);
   }
 
   function handleTemplateExpressions(template, expressions) {
-    evaluateExpression()
+    // const bundledData = bundleAppData();
     let expressionId = 1;
     expressions.forEach(exp => {
       const trimmedExp = exp.slice(2, -2).trim();
-      template = template.replace(exp, `<span data-exp-id="${expressionId}"></span>`);
+      const val = evaluateExpression(trimmedExp);
+      template = template.replace(exp, `<span data-exp-id="${expressionId}">${val}</span>`);
       vDom.expressions[expressionId] = trimmedExp;
       expressionId += 1;
     });
@@ -65,7 +105,7 @@ const TemplateComp = (() => {
 
     const listenDirectives = template.match(reDict.tagsWithEvents);
     if (listenDirectives) {
-      handleTemplateListenDirectives(template, listenDirectives);
+      template = handleTemplateListenDirectives(template, listenDirectives);
     }
 
     this._vDom = Object.assign(this._vDom, vDom);
