b490f1a69d7bec42e50df5df077f5cbcae58676a
Jacob Albright <jacob.albright23@gmail.com>
2018-10-03 20:41:20 -0500

    Have successfully implemented reactive computed properties

--- computed-properties.js
+++ computed-properties.js
@@ -1,102 +1,105 @@
 
 
-function handleData(vDom, data) {
-  Object.keys(data).forEach(key => {
-    Object.defineProperty(vDom, key, {
-      get: () => data[key],
-      set: val => {
-        data[key] = val;
-        notifyVDom(key);
-      },
-    });
-  });
-}
-
-function notifyVDom() {
-
-}
-
-function handleComputed(data, computed) {
-  Object.keys(data)
-}
-
-
-function withComputed(data, computed) {
-
-  const vDom = {
-
-  };
-
-  handleData(data);
-  handleComputed(computed);
-
-}
-
-
-class WithComputedProps {
+class ComputedPropertyWatcher {
   constructor({ data, computed }) {
-    this.test = 'Hello'
-    this.computedDependencies = {
-      
+    this._data = data;
+    this._computed = computed;
+    this._vdom = {
+      compDependencies: {
+
+      },
+      data: {
+
+      },
     };
-    this.allData = this.setAllData(data, computed);
-    console.log(this.allData);
-    
+
+    this.wrapData();
+    this.wrapComputed();
   }
 
-  setAllData(data, computed) {
-    const result = {};
-    const dataKeys = Object.keys(data)
-    
-    dataKeys.forEach(key => {
-      Object.defineProperty(result, key, {
-        get: () => data[key],
+  wrapData() {
+    Object.keys(this._data).forEach(key => {
+      Object.defineProperty(this._vdom.data, key, {
+        get: () => this._data[key],
         set: val => {
-          data[key] = val;
-          this.notifyDependencies(key)
+          this._data[key] = val;
+          this.notifyDependencies(key);
         }
       });
     });
-
-    this.setDepRefs(dataKeys, computed);
-    Object.assign(result, this.evaluateComputed(data, computed));
-    
   }
 
-  evaluateComputed(data, computed) {    
-    return Object.keys(computed).reduce((accum, key) => {
-      accum[key] = computed[key].call(data);
-      return accum;
-    }, {});
+  wrapComputed() {
+    Object.keys(this._computed).forEach(key => {
+      const fnStr = this._computed[key].toString();
+      const fnBodyStr = fnStr.slice(fnStr.indexOf('{') + 1, fnStr.lastIndexOf('}'));
+      this._vdom.compDependencies[key] = Object.keys(this._data).reduce((accum, dataKey) => {
+        if (fnBodyStr.match('this.' + dataKey)) {
+          accum.push(dataKey);
+        }
+        return accum;
+      }, []);
+    });
+
+    // evaluate computed properties
+    Object.keys(this._computed).forEach(key => {
+      this._vdom.data[key] = this._computed[key].call(this._vdom.data);
+    });
   }
+
 
   setDepRefs(dataKeys, computed) {
     let compPropsCount = 1;
     Object.keys(computed).forEach(key => {
-      const fnStr = computed[key].toString();      
+      const fnStr = computed[key].toString();
       const fnBodyStr = fnStr.slice(fnStr.indexOf('{') + 1, fnStr.lastIndexOf('}'));
       this.computedDependencies[compPropsCount] = dataKeys.reduce((accum, dataKey) => {
         if (fnBodyStr.match('this.' + dataKey)) {
-          accum.push('this.'+dataKey)
+          accum.push('this.' + dataKey)
         }
         return accum;
       }, []);
       compPropsCount += 1;
     });
   }
+
+  notifyDependencies(dataKey) {
+    const compDependencies = this._vdom.compDependencies
+    Object.keys(compDependencies).forEach(depKey => {
+      if (compDependencies[depKey].includes(dataKey)) {        
+        this._vdom.data[depKey] = this._computed[depKey].call(this._vdom.data);
+      }
+    });
+  }
+
+
 }
 
-
-
-const instance = new WithComputedProps({
+let counter = 0
+const instance = new ComputedPropertyWatcher({
   data: {
     name: 'Jacob',
-    age: 9,
+    num: 1,
+    num2: 55,
   },
   computed: {
     nameToUpper() {
-      return this.name.toUpperCase() + this.age;
+      return this.name.toUpperCase();
     },
-  },
-});
+    sumOfNums() {
+      return this.num + this.num2;
+    }
+  }
+})
+
+console.log(instance._vdom.data);
+
+instance._vdom.data.name = 'Caroline'
+instance._vdom.data.num2 = 22
+
+console.log(instance._vdom.data);
+
+instance._vdom.data.num2 = 12
+
+console.log(instance._vdom.data);
 
